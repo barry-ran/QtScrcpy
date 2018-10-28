6e95a8d7ac1d1122d2cce3a56b37e3cebd7baf69
Barry <870709864@qq.com>
2018-10-28 23:19:38 +0800

    测试opengl es2.0

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    //QApplication::setAttribute(Qt::AA_UseOpenGLES);
+    //QApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+
     QApplication a(argc, argv);
 
     qputenv("QTSCRCPY_ADB_PATH", "G:\\mygitcode\\QtScrcpy\\src\\adb.exe");
