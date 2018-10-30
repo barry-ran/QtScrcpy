fafffac92b431057d6f208654fbbf44bb3bdb8cc
rankun <rankun@hujiang.com>
2018-10-30 13:47:27 +0800

    完善fps统计

--- src/fpscounter.cpp
+++ src/fpscounter.cpp
@@ -1,8 +1,9 @@
+#include <QTimerEvent>
 #include <QDebug>
 
 #include "fpscounter.h"
 
-FpsCounter::FpsCounter()
+FpsCounter::FpsCounter(QObject* parent) : QObject(parent)
 {
 
 }
@@ -12,63 +13,59 @@ FpsCounter::~FpsCounter()
 
 }
 
-void FpsCounter::fpsCounterInit()
+void FpsCounter::start()
 {
-    m_started = false;
-    // no need to initialize the other fields, they are meaningful only when
-    // started is true
+    resetCounter();
+    startCounterTimer();
 }
 
-void FpsCounter::fpsCounterStart()
-{
-    m_started = true;
-    m_timeCounter.start();
-    m_rendered = 0;
-#ifdef SKIP_FRAMES
-    m_skipped = 0;
-#endif
+void FpsCounter::stop()
+{    
+    stopCounterTimer();
+    resetCounter();
 }
 
-void FpsCounter::fpsCounterStop()
+bool FpsCounter::isStarted()
 {
-    m_started = false;
+    return m_counterTimer;
 }
 
-void FpsCounter::fpsCounterAddRenderedFrame()
+void FpsCounter::addRenderedFrame()
 {
-    checkExpired();
     m_rendered++;
 }
 
-void FpsCounter::checkExpired()
+void FpsCounter::addSkippedFrame()
 {
-    if (m_timeCounter.elapsed() >= 1000) {
-        displayFps();
-        m_timeCounter.restart();
-        m_rendered = 0;
-#ifdef SKIP_FRAMES
-        m_skipped = 0;
-#endif
-       }
-}
-
-void FpsCounter::displayFps()
-{
-#ifdef SKIP_FRAMES
-    if (m_skipped) {
-        //qInfo << "%d fps (+%d frames skipped)", m_rendered, m_skipped);
-    } else {
-#endif
-    //qInfo m_rendered << "fps";
-#ifdef SKIP_FRAMES
-    }
-#endif
-}
-
-#ifdef SKIP_FRAMES
-void FpsCounter::fpsCounterAddSkippedFrame()
-{
-    checkExpired();
     m_skipped++;
 }
-#endif
+
+void FpsCounter::timerEvent(QTimerEvent *event)
+{
+    if (event && m_counterTimer == event->timerId()) {
+        m_curRendered = m_rendered;
+        m_curSkipped = m_skipped;
+        resetCounter();
+        qInfo("FPS:%d Discard:%d", m_curRendered, m_skipped);
+    }
+}
+
+void FpsCounter::startCounterTimer()
+{
+    stopCounterTimer();
+    m_counterTimer = startTimer(1000);
+}
+
+void FpsCounter::stopCounterTimer()
+{
+    if (m_counterTimer) {
+        killTimer(m_counterTimer);
+        m_counterTimer = 0;
+    }
+}
+
+void FpsCounter::resetCounter()
+{
+    m_rendered = 0;
+    m_skipped = 0;
+}
