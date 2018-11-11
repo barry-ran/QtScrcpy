accd23172ace245443a9761c0d64f24116cbbed7
Barry <870709864@qq.com>
2018-11-11 22:32:42 +0800

    游戏方向盘控制逻辑初步

--- src/inputcontrol/inputconvertgame.h
+++ src/inputcontrol/inputconvertgame.h
@@ -1,8 +1,10 @@
 #ifndef INPUTCONVERTGAME_H
 #define INPUTCONVERTGAME_H
 
+#include <QPointF>
 #include "inputconvertbase.h"
 
+#define MULTI_TOUCH_MAX_NUM 10
 class InputConvertGame : public InputConvertBase
 {
 public:
@@ -12,6 +14,44 @@ public:
     void mouseEvent(const QMouseEvent* from, const QSize& frameSize, const QSize& showSize);
     void wheelEvent(const QWheelEvent* from, const QSize& frameSize, const QSize& showSize);
     void keyEvent(const QKeyEvent* from, const QSize& frameSize, const QSize& showSize);
+
+protected:
+    void updateSize(const QSize& frameSize, const QSize& showSize);
+    void sendTouchDownEvent(int id, QPointF pos);
+    void sendTouchMoveEvent(int id, QPointF pos);
+    void sendTouchUpEvent(int id, QPointF pos);
+    void sendTouchEvent(int id, QPointF pos, AndroidMotioneventAction action);
+    QPointF calcAbsolutePos(QPointF relativePos);
+
+    // multi touch id
+    int attachTouchID(int key);
+    void detachTouchID(int key);
+    int getTouchID(int key);
+
+    // steer wheel
+    bool isSteerWheelKeys(const QKeyEvent* from);
+    void processSteerWheel(const QKeyEvent* from);
+
+private:
+    enum SteerWheelDirection {
+        SWD_UP = 0,
+        SWD_RIGHT,
+        SWD_DOWN,
+        SWD_LEFT,
+    };
+
+private:
+    QSize m_frameSize;
+    QSize m_showSize;
+
+    int multiTouchID[MULTI_TOUCH_MAX_NUM] = { 0 };
+
+    QPointF m_steerWheelPos = {0.16f, 0.75f};
+    float m_steerWheelOffset = 0.1f;
+    // order by SteerWheelDirection(up right down left)
+    int m_steerWheelKeys[4] = {Qt::Key_W, Qt::Key_D, Qt::Key_S, Qt::Key_A};
+    int m_steerWheelKeysNum = 0;
+    int m_steerWheelFirstTouchKey = 0;
 };
 
 #endif // INPUTCONVERTGAME_H
