1c91adefcc5979c08164f03cbecc099e0b7b3e16
rankun <rankun@hujiang.com>
2018-11-14 14:08:50 +0800

    修改项目结构

---
+++ QtScrcpy/inputcontrol/inputconvertnormal.h
@@ -0,0 +1,22 @@
+#ifndef INPUTCONVERT_H
+#define INPUTCONVERT_H
+
+#include "inputconvertbase.h"
+
+class InputConvertNormal : public InputConvertBase
+{
+public:
+    InputConvertNormal();
+    virtual ~InputConvertNormal();
+
+    virtual void mouseEvent(const QMouseEvent* from, const QSize& frameSize, const QSize& showSize);
+    virtual void wheelEvent(const QWheelEvent* from, const QSize& frameSize, const QSize& showSize);
+    virtual void keyEvent(const QKeyEvent* from, const QSize& frameSize, const QSize& showSize);
+
+private:
+    AndroidMotioneventButtons convertMouseButtons(Qt::MouseButtons buttonState);
+    AndroidKeycode convertKeyCode(int key, Qt::KeyboardModifiers modifiers);
+    AndroidMetastate convertMetastate(Qt::KeyboardModifiers modifiers);
+};
+
+#endif // INPUTCONVERT_H
