968de0d20c727ac99c0f72c920904d79df3b87c2
Barry <870709864@qq.com>
2018-11-24 09:20:13 +0800

    代码优化

--- QtScrcpy/main.cpp
+++ QtScrcpy/main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
 #ifdef Q_OS_WIN32
-    qputenv("QTSCRCPY_ADB_PATH", "..\\..\\..\\third_party\\adb\\adb.exe");
-    qputenv("QTSCRCPY_SERVER_PATH", "..\\..\\..\\third_party\\scrcpy-server.jar");
+    qputenv("QTSCRCPY_ADB_PATH", "../../../third_party/adb/adb.exe");
+    qputenv("QTSCRCPY_SERVER_PATH", "../../../third_party/scrcpy-server.jar");
 #endif
 
     //加载样式表
