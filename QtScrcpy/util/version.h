c24fbd9638bcd4f2c885deaf350c05e4d3902b72
Barry <870709864@qq.com>
2019-06-22 09:59:26 +0800

    add:windows平台版本说明

---
+++ QtScrcpy/util/version.h
@@ -0,0 +1,16 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define PRODUCT_ICON           "QtScrcpy.ico" // 图标
+
+#define FILE_VERSION           1,0,1,0   // 文件版本
+#define FILE_VERSION_STR       "1.0.1.0"
+#define PRODUCT_VERSION        1,0,1,0   // 产品版本
+#define PRODUCT_VERSION_STR    "1.0.1.0"
+#define COMPANY_NAME           "RanKun"
+#define INTERNAL_NAME          "QtScrcpy.exe"
+#define FILE_DESCRIPTION       "android real time projection software"  // 文件说明
+#define LEGAL_COPYRIGHT        "Copyright (C) RanKun 2018-2028. All rights reserved." // 版权
+#define PRODUCT_NAME           "QtScrcpy"        // 产品名称
+
+#endif // VERSION_H
