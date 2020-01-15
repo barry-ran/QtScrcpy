65a7cb746768dbb7f9d89f041279cb30615e0a1d
rankun <rankun@bytedance.com>
2020-01-15 12:34:44 +0800

    feat: Reduce latency by 1 frame
    To packetize the H.264 raw stream, av_parser_parse2() (called by
    av_read_frame()) knows that it has received a full frame only after it
    has received some data for the next frame. As a consequence, the client
    always waited until the next frame before sending the current frame to
    the decoder!
    
    On the device side, we know packets boundaries. To reduce latency,
    make the device always transmit the "frame meta" to packetize the stream
    manually (it was already implemented to send PTS, but only enabled on
    recording).
    
    On the client side, replace av_read_frame() by manual packetizing and
    parsing.
    
    <https://stackoverflow.com/questions/50682518/replacing-av-read-frame-to-reduce-delay>
    <https://trac.ffmpeg.org/ticket/3354>

--- QtScrcpy/device/stream/stream.h
+++ QtScrcpy/device/stream/stream.h
@@ -18,17 +18,6 @@ class Stream : public QThread
 {
     Q_OBJECT
 public:
-    typedef struct FrameMeta {
-        quint64 pts;
-        struct FrameMeta* next;
-    } FrameMeta;
-
-    typedef struct ReceiverState {
-        // meta (in order) for frames not consumed yet
-        FrameMeta* frameMetaQueue;
-        qint32 remaining; // remaining bytes to receive for the current frame
-    } ReceiverState;
-
     Stream(QObject *parent = Q_NULLPTR);
     virtual ~Stream();
 
@@ -36,28 +25,36 @@ public:
     static bool init();
     static void deInit();
 
-    void setDecoder(Decoder* vb);
-    void setVideoSocket(VideoSocket* deviceSocket);
+    void setDecoder(Decoder* decoder);
     void setRecoder(Recorder* recorder);
+    void setVideoSocket(VideoSocket* deviceSocket);
     qint32 recvData(quint8* buf, qint32 bufSize);
     bool startDecode();
     void stopDecode();
-    ReceiverState* getReceiverState();
 
 signals:
     void onStreamStop();
 
 protected:
     void run();
+    bool recvPacket(AVPacket* packet);
+    bool pushPacket(AVPacket* packet);
+    bool processConfigPacket(AVPacket *packet);
+    bool parse(AVPacket *packet);
+    bool processFrame(AVPacket *packet);
 
 private:
     QPointer<VideoSocket> m_videoSocket;
-    std::atomic_bool m_quit;
-
     // for recorder
     Recorder* m_recorder = Q_NULLPTR;
-    ReceiverState m_receiverState;
     Decoder* m_decoder = Q_NULLPTR;
+
+    AVCodecContext* m_codecCtx = Q_NULLPTR;
+    AVCodecParserContext *m_parser = Q_NULLPTR;
+    // successive packets may need to be concatenated, until a non-config
+    // packet is available
+    bool m_hasPending = false;
+    AVPacket m_pending;
 };
 
 #endif // STREAM_H
