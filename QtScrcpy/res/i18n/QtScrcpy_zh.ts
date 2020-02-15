2e681600abd77b5fca8116e4490f999c66128300
rankun <rankun@bytedance.com>
2020-02-15 18:34:55 +0800

    feat: change native to original

--- QtScrcpy/res/i18n/QtScrcpy_zh.ts
+++ QtScrcpy/res/i18n/QtScrcpy_zh.ts
@@ -16,22 +16,22 @@
         <translation type="vanished">文件传输失败</translation>
     </message>
     <message>
-        <location filename="../../device/device.cpp" line="127"/>
+        <location filename="../../device/device.cpp" line="134"/>
         <source>install apk</source>
         <translation>安装apk</translation>
     </message>
     <message>
-        <location filename="../../device/device.cpp" line="129"/>
+        <location filename="../../device/device.cpp" line="136"/>
         <source>file transfer</source>
         <translation>文件传输</translation>
     </message>
     <message>
-        <location filename="../../device/device.cpp" line="132"/>
+        <location filename="../../device/device.cpp" line="139"/>
         <source>wait current %1 to complete</source>
         <translation>等待当前%1完成</translation>
     </message>
     <message>
-        <location filename="../../device/device.cpp" line="135"/>
+        <location filename="../../device/device.cpp" line="142"/>
         <source>%1 complete, save in %2</source>
         <translation>%1完成,保存在%2</translation>
     </message>
@@ -41,7 +41,7 @@
         <translation type="vanished">%1完成\n 保存在 %2</translation>
     </message>
     <message>
-        <location filename="../../device/device.cpp" line="138"/>
+        <location filename="../../device/device.cpp" line="145"/>
         <source>%1 failed</source>
         <translation>%1 失败</translation>
     </message>
@@ -197,6 +197,11 @@
         <source>refresh devices</source>
         <translation>刷新设备列表</translation>
     </message>
+    <message>
+        <location filename="../../dialog.cpp" line="93"/>
+        <source>original</source>
+        <translation>原始</translation>
+    </message>
 </context>
 <context>
     <name>ToolForm</name>
@@ -289,7 +294,7 @@
         <translation type="vanished">文件传输失败</translation>
     </message>
     <message>
-        <location filename="../../device/ui/videoform.cpp" line="413"/>
+        <location filename="../../device/ui/videoform.cpp" line="415"/>
         <source>file does not exist</source>
         <translation>文件不存在</translation>
     </message>
