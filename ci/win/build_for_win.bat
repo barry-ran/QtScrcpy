041c78297bd77eaa53e9f1ce7d538e58b8fcc321
rankun <rankun@bytedance.com>
2020-02-20 12:19:42 +0800

    1

--- ci/win/build_for_win.bat
+++ ci/win/build_for_win.bat
@@ -88,11 +88,14 @@ if /i %debug_mode% == "true" (
 :: qmake ../../all.pro -spec win32-msvc "CONFIG+=debug" "CONFIG+=qml_debug"
 :: qmake ../../all.pro %qmake_params%
 :qmake ../../all.pro
+if not %errorlevel%==0 (
+    echo "qmake failed"
+    goto return
+)
 
 nmake
-
 if not %errorlevel%==0 (
-    echo "qmake build failed"
+    echo "nmake build failed"
     goto return
 )
 
