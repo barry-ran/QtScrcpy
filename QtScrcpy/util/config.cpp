a36830305fa2d04f92e0c71f58e5e157cbd445b9
Barry <870709864@qq.com>
2020-02-22 19:38:38 +0800

    Revert "feat: mac action (#86)" (#87)
    This reverts commit 327dd9ea68b002f6de1fa2ae76ecd1c65f67b396.

--- QtScrcpy/util/config.cpp
+++ QtScrcpy/util/config.cpp
@@ -6,9 +6,11 @@
 
 #define GROUP_COMMON "common"
 
-// config
 #define COMMON_TITLE_KEY "WindowTitle"
 #define COMMON_TITLE_DEF QCoreApplication::applicationName()
+
+#define COMMON_RECORD_KEY "RecordPath"
+#define COMMON_RECORD_DEF ""
 
 #define COMMON_PUSHFILE_KEY "PushFilePath"
 #define COMMON_PUSHFILE_DEF "/sdcard/"
@@ -31,30 +33,12 @@
 #define COMMON_RENDER_EXPIRED_FRAMES_KEY "RenderExpiredFrames"
 #define COMMON_RENDER_EXPIRED_FRAMES_DEF 0
 
-// user data
-#define COMMON_RECORD_KEY "RecordPath"
-#define COMMON_RECORD_DEF ""
-
-#define COMMON_BITRATE_INDEX_KEY "BitRateIndex"
-#define COMMON_BITRATE_INDEX_DEF 2
-
-#define COMMON_MAX_SIZE_INDEX_KEY "MaxSizeIndex"
-#define COMMON_MAX_SIZE_INDEX_DEF 2
-
-#define COMMON_RECORD_FORMAT_INDEX_KEY "RecordFormatIndex"
-#define COMMON_RECORD_FORMAT_INDEX_DEF 0
-
-// 最大尺寸 录制格式
-
 QString Config::s_configPath = "";
 
 Config::Config(QObject *parent) : QObject(parent)
 {
-    m_settings = new QSettings(getConfigPath() + "/config.ini", QSettings::IniFormat);
+    m_settings = new QSettings(getConfigPath(), QSettings::IniFormat);
     m_settings->setIniCodec("UTF-8");
-
-    m_userData = new QSettings(getConfigPath() + "/userdata.ini", QSettings::IniFormat);
-    m_userData->setIniCodec("UTF-8");
 }
 
 Config &Config::getInstance()
@@ -68,8 +52,8 @@ const QString& Config::getConfigPath()
     if (s_configPath.isEmpty()) {
         s_configPath = QString::fromLocal8Bit(qgetenv("QTSCRCPY_CONFIG_PATH"));
         QFileInfo fileInfo(s_configPath);
-        if (s_configPath.isEmpty() || !fileInfo.isDir()) {
-            s_configPath = QCoreApplication::applicationDirPath() + "/config";
+        if (s_configPath.isEmpty() || !fileInfo.isFile()) {
+            s_configPath = QCoreApplication::applicationDirPath() + "/config/config.ini";
         }
     }
     return s_configPath;
@@ -78,65 +62,17 @@ const QString& Config::getConfigPath()
 QString Config::getRecordPath()
 {
     QString record;
-    m_userData->beginGroup(GROUP_COMMON);
-    record = m_userData->value(COMMON_RECORD_KEY, COMMON_RECORD_DEF).toString();
-    m_userData->endGroup();
+    m_settings->beginGroup(GROUP_COMMON);
+    record = m_settings->value(COMMON_RECORD_KEY, COMMON_RECORD_DEF).toString();
+    m_settings->endGroup();
     return record;
 }
 
 void Config::setRecordPath(const QString &path)
 {
-    m_userData->beginGroup(GROUP_COMMON);
-    m_userData->setValue(COMMON_RECORD_KEY, path);
-    m_userData->endGroup();
-}
-
-int Config::getBitRateIndex()
-{
-    int bitRateIndex;
-    m_userData->beginGroup(GROUP_COMMON);
-    bitRateIndex = m_userData->value(COMMON_BITRATE_INDEX_KEY, COMMON_BITRATE_INDEX_DEF).toInt();
-    m_userData->endGroup();
-    return bitRateIndex;
-}
-
-void Config::setBitRateIndex(int bitRateIndex)
-{
-    m_userData->beginGroup(GROUP_COMMON);
-    m_userData->setValue(COMMON_BITRATE_INDEX_KEY, bitRateIndex);
-    m_userData->endGroup();
-}
-
-int Config::getMaxSizeIndex()
-{
-    int maxSizeIndex;
-    m_userData->beginGroup(GROUP_COMMON);
-    maxSizeIndex = m_userData->value(COMMON_MAX_SIZE_INDEX_KEY, COMMON_MAX_SIZE_INDEX_DEF).toInt();
-    m_userData->endGroup();
-    return maxSizeIndex;
-}
-
-void Config::setMaxSizeIndex(int maxSizeIndex)
-{
-    m_userData->beginGroup(GROUP_COMMON);
-    m_userData->setValue(COMMON_MAX_SIZE_INDEX_KEY, maxSizeIndex);
-    m_userData->endGroup();
-}
-
-int Config::getRecordFormatIndex()
-{
-    int recordFormatIndex;
-    m_userData->beginGroup(GROUP_COMMON);
-    recordFormatIndex = m_userData->value(COMMON_RECORD_FORMAT_INDEX_KEY, COMMON_RECORD_FORMAT_INDEX_DEF).toInt();
-    m_userData->endGroup();
-    return recordFormatIndex;
-}
-
-void Config::setRecordFormatIndex(int recordFormatIndex)
-{
-    m_userData->beginGroup(GROUP_COMMON);
-    m_userData->setValue(COMMON_RECORD_FORMAT_INDEX_KEY, recordFormatIndex);
-    m_userData->endGroup();
+    m_settings->beginGroup(GROUP_COMMON);
+    m_settings->setValue(COMMON_RECORD_KEY, path);
+    m_settings->endGroup();
 }
 
 QString Config::getServerVersion()
