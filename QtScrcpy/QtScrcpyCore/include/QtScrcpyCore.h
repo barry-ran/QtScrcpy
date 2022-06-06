a7c535cf685f12c9a53bfde56972005870546ea2
Barry <870709864@qq.com>
2022-06-06 08:41:24 +0800

    fix: build error on mac

--- QtScrcpy/QtScrcpyCore/include/QtScrcpyCore.h
+++ QtScrcpy/QtScrcpyCore/include/QtScrcpyCore.h
@@ -15,9 +15,18 @@ protected:
     }
 
 public:
-    virtual void onFrame(int width, int height, uint8_t* dataY, uint8_t* dataU, uint8_t* dataV, int linesizeY, int linesizeU, int linesizeV) {}
-    virtual void updateFPS(quint32 fps) {}
-    virtual void grabCursor(bool grab) {}
+    virtual void onFrame(int width, int height, uint8_t* dataY, uint8_t* dataU, uint8_t* dataV, int linesizeY, int linesizeU, int linesizeV) {
+        Q_UNUSED(width);
+        Q_UNUSED(height);
+        Q_UNUSED(dataY);
+        Q_UNUSED(dataU);
+        Q_UNUSED(dataV);
+        Q_UNUSED(linesizeY);
+        Q_UNUSED(linesizeU);
+        Q_UNUSED(linesizeV);
+    }
+    virtual void updateFPS(quint32 fps) { Q_UNUSED(fps); }
+    virtual void grabCursor(bool grab) {Q_UNUSED(grab);}
 };
 
 class IDevice : public QObject {
