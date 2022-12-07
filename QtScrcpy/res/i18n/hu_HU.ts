066324a6d1bf4be522aa293aca4e1bd78cebaaa0
gidano <gidano@gmail.com>
2022-12-07 19:38:26 +0100

    Hungarian translate

---
+++ QtScrcpy/res/i18n/hu_HU.ts
@@ -0,0 +1,287 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>ToolForm</name>
+    <message>
+        <source>Tool</source>
+        <translation>Eszköz</translation>
+    </message>
+    <message>
+        <source>home</source>
+        <translation>kezdőlap</translation>
+    </message>
+    <message>
+        <source>menu</source>
+        <translation>menü</translation>
+    </message>
+    <message>
+        <source>touch switch</source>
+        <translation>érintő kapcsoló</translation>
+    </message>
+    <message>
+        <source>power</source>
+        <translation>bekapcsoló</translation>
+    </message>
+    <message>
+        <source>close screen</source>
+        <translation>képernyő bezárása</translation>
+    </message>
+    <message>
+        <source>volume down</source>
+        <translation>hangerő le</translation>
+    </message>
+    <message>
+        <source>volume up</source>
+        <translation>hangerő fel</translation>
+    </message>
+    <message>
+        <source>return</source>
+        <translation>vissza</translation>
+    </message>
+    <message>
+        <source>app switch</source>
+        <translation>app váltó</translation>
+    </message>
+    <message>
+        <source>open screen</source>
+        <translation>képernyő megnyitás</translation>
+    </message>
+    <message>
+        <source>full screen</source>
+        <translation>teljes képernyő</translation>
+    </message>
+    <message>
+        <source>group control</source>
+        <translation>csoport vezérlés</translation>
+    </message>
+    <message>
+        <source>expand notify</source>
+        <translation>értesítés kibontása</translation>
+    </message>
+    <message>
+        <source>screen shot</source>
+        <translation>képernyőkép</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>quit</source>
+        <translation>kilépés</translation>
+    </message>
+    <message>
+        <source>show</source>
+        <translation>mutat</translation>
+    </message>
+    <message>
+        <source>Hidden here!</source>
+        <translation>Itt elrejtve!</translation>
+    </message>
+    <message>
+        <source>no lock</source>
+        <translation>nem zárol</translation>
+    </message>
+    <message>
+        <source>Notice</source>
+        <translation>Értesítés</translation>
+    </message>
+    <message>
+        <source>original</source>
+        <translation>eredeti</translation>
+    </message>
+    <message>
+        <source>select path</source>
+        <translation>válasszon útvonalat</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <source>start server</source>
+        <translation>indítsa el a szervert</translation>
+    </message>
+    <message>
+        <source>apply</source>
+        <translation>alkalmaz</translation>
+    </message>
+    <message>
+        <source>clear</source>
+        <translation>törlés</translation>
+    </message>
+    <message>
+        <source>Wireless</source>
+        <translation>Vez. nélküli</translation>
+    </message>
+    <message>
+        <source>get device IP</source>
+        <translation>eszköz IP lekérés</translation>
+    </message>
+    <message>
+        <source>Start Config</source>
+        <translation>konfig. indítása</translation>
+    </message>
+    <message>
+        <source>stop all server</source>
+        <translation>minden szerver leállítása</translation>
+    </message>
+    <message>
+        <source>device name:</source>
+        <translation>eszköz neve:</translation>
+    </message>
+    <message>
+        <source>Simple Mode</source>
+        <translation>Egyszerű mód</translation>
+    </message>
+    <message>
+        <source>install sndcpy</source>
+        <translation>sndcpy telepítés</translation>
+    </message>
+    <message>
+        <source>stop audio</source>
+        <translation>hang leállítása</translation>
+    </message>
+    <message>
+        <source>adb command:</source>
+        <translation>adb parancs:</translation>
+    </message>
+    <message>
+        <source>device serial:</source>
+        <translation>eszköz sor.szám:</translation>
+    </message>
+    <message>
+        <source>Use Simple Mode</source>
+        <translation>Egyszerű mód használata</translation>
+    </message>
+    <message>
+        <source>start audio</source>
+        <translation>hang indítása</translation>
+    </message>
+    <message>
+        <source>bit rate:</source>
+        <translation>bitarány:</translation>
+    </message>
+    <message>
+        <source>wireless connect</source>
+        <translation>vez.nélküli kapcsolat</translation>
+    </message>
+    <message>
+        <source>refresh devices</source>
+        <translation>eszközök frissítése</translation>
+    </message>
+    <message>
+        <source>USB line</source>
+        <translation>USB vonal</translation>
+    </message>
+    <message>
+        <source>auto update</source>
+        <translation>auto frissítés</translation>
+    </message>
+    <message>
+        <source>stop server</source>
+        <translation>szerver leállítás</translation>
+    </message>
+    <message>
+        <source>frameless</source>
+        <translation>keret nélkül</translation>
+    </message>
+    <message>
+        <source>terminate</source>
+        <translation>befejez</translation>
+    </message>
+    <message>
+        <source>start adbd</source>
+        <translation>adbd indítás</translation>
+    </message>
+    <message>
+        <source>background record</source>
+        <translation>rögzítés a háttérben</translation>
+    </message>
+    <message>
+        <source>update name</source>
+        <translation>név frissítése</translation>
+    </message>
+    <message>
+        <source>select path</source>
+        <translation>elérési útvonal</translation>
+    </message>
+    <message>
+        <source>USB Connect</source>
+        <translation>USB kapcsolat</translation>
+    </message>
+    <message>
+        <source>reverse connection</source>
+        <translation>fordított csatlakozás</translation>
+    </message>
+    <message>
+        <source>max size:</source>
+        <translation>max méret:</translation>
+    </message>
+    <message>
+        <source>stay awake</source>
+        <translation>maradjon ébren</translation>
+    </message>
+    <message>
+        <source>screen-off</source>
+        <translation>képernyő ki</translation>
+    </message>
+    <message>
+        <source>refresh script</source>
+        <translation>szkript frissítése</translation>
+    </message>
+    <message>
+        <source>execute</source>
+        <translation>végrehajt</translation>
+    </message>
+    <message>
+        <source>Double click to connect:</source>
+        <translation>Kattintson duplán a csatlakozáshoz:</translation>
+    </message>
+    <message>
+        <source>lock orientation:</source>
+        <translation>zárolt tájolás:</translation>
+    </message>
+    <message>
+        <source>WIFI Connect</source>
+        <translation>WIFI kapcsolat</translation>
+    </message>
+    <message>
+        <source>wireless disconnect</source>
+        <translation>vez.nélküli kapcsolat megszakítása</translation>
+    </message>
+    <message>
+        <source>record screen</source>
+        <translation>képernyő felvétel</translation>
+    </message>
+    <message>
+        <source>show fps</source>
+        <translation>fps megjelenítés</translation>
+    </message>
+    <message>
+        <source>record save path:</source>
+        <translation>mentett felvétel helye:</translation>
+    </message>
+    <message>
+        <source>always on top</source>
+        <translation>mindig felül</translation>
+    </message>
+    <message>
+        <source>record format：</source>
+        <translation>felvétel formátum:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>This software is completely open source and free. Use it at your own risk. You can download it at the following address:</source>
+        <translation>Ez a szoftver teljesen nyílt forráskódú és ingyenes. Használja saját felelősségére. Letölthető az alábbi címről:</translation>
+    </message>
+</context>
+<context>
+    <name>VideoForm</name>
+    <message>
+        <source>file does not exist</source>
+        <translation>a fájl nem létezik</translation>
+    </message>
+</context>
+</TS>
