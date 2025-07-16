c969dc93bfc167960d889d1bf7eb7065da54ca82
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2025-07-16 17:52:41 +0900

    Add Japanese Language

---
+++ QtScrcpy/res/i18n/ja_JP.ts
@@ -0,0 +1,321 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS []>
+<TS version="2.1" language="ja_JP">
+  <context>
+    <name>Dialog</name>
+    <message>
+      <source>show</source>
+      <translation>表示</translation>
+    </message>
+    <message>
+      <source>quit</source>
+      <translation>終了</translation>
+    </message>
+    <message>
+      <source>original</source>
+      <translation>オリジナル</translation>
+    </message>
+    <message>
+      <source>no lock</source>
+      <translation>ロックなし</translation>
+    </message>
+    <message>
+      <source>Notice</source>
+      <translation>お知らせ</translation>
+    </message>
+    <message>
+      <source>Hidden here!</source>
+      <translation>ここに隠れています！</translation>
+    </message>
+    <message>
+      <source>select path</source>
+      <translation>パスを選択</translation>
+    </message>
+    <message>
+      <source>Clear History</source>
+      <translation>履歴を消去</translation>
+    </message>
+  </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <source>This software is completely open source and free. Use it at your own risk. You can download it at the following address:</source>
+      <translation>このソフトウェアはオープンソースで完全無料です。自己責任でご利用ください。以下のアドレスからダウンロードできます:</translation>
+    </message>
+    <message>
+      <source>QuickMirror</source>
+      <translation>クイックミラー</translation>
+    </message>
+    <message>
+      <source>If you need more professional batch control mirror software, you can try the following software:</source>
+      <translation>より高度なバッチ制御が可能なミラーソフトウェアが必要な場合は、次のソフトウェアをお試しください:</translation>
+    </message>
+    <message>
+      <source>If you need more professional game keymap mirror software, you can try the following software:</source>
+      <translation>より高度なゲームキーマップが可能なミラーソフトウェアが必要な場合は、次のソフトウェアをお試しください:</translation>
+    </message>
+    <message>
+      <source>QuickAssistant</source>
+      <translation>クイックアシスタント</translation>
+    </message>
+    <message>
+      <source>You can contact me with telegram &lt;https://t.me/+Ylf_5V_rDCMyODQ1&gt;</source>
+      <translation>Telegram で連絡ができます &lt;https://t.me/+Ylf_5V_rDCMyODQ1&gt;</translation>
+    </message>
+  </context>
+  <context>
+    <name>ToolForm</name>
+    <message>
+      <source>Tool</source>
+      <translation>ツール</translation>
+    </message>
+    <message>
+      <source>full screen</source>
+      <translation>フルスクリーン</translation>
+    </message>
+    <message>
+      <source>expand notify</source>
+      <translation>通知を展開</translation>
+    </message>
+    <message>
+      <source>touch switch</source>
+      <translation>タッチ切り替え</translation>
+    </message>
+    <message>
+      <source>close screen</source>
+      <translation>画面を閉じる</translation>
+    </message>
+    <message>
+      <source>power</source>
+      <translation>電源</translation>
+    </message>
+    <message>
+      <source>volume up</source>
+      <translation>音量を上げる</translation>
+    </message>
+    <message>
+      <source>volume down</source>
+      <translation>音量を下げる</translation>
+    </message>
+    <message>
+      <source>app switch</source>
+      <translation>アプリを切り替え</translation>
+    </message>
+    <message>
+      <source>menu</source>
+      <translation>メニュー</translation>
+    </message>
+    <message>
+      <source>home</source>
+      <translation>ホーム</translation>
+    </message>
+    <message>
+      <source>return</source>
+      <translation>戻る</translation>
+    </message>
+    <message>
+      <source>screen shot</source>
+      <translation>スクリーンショット</translation>
+    </message>
+    <message>
+      <source>open screen</source>
+      <translation>画面を開く</translation>
+    </message>
+    <message>
+      <source>group control</source>
+      <translation>グループコントロール</translation>
+    </message>
+  </context>
+  <context>
+    <name>VideoForm</name>
+    <message>
+      <source>file does not exist</source>
+      <translation>ファイルが存在しません</translation>
+    </message>
+  </context>
+  <context>
+    <name>Widget</name>
+    <message>
+      <source>Wireless</source>
+      <translation>ワイヤレス</translation>
+    </message>
+    <message>
+      <source>wireless connect</source>
+      <translation>ワイヤレスで接続</translation>
+    </message>
+    <message>
+      <source>wireless disconnect</source>
+      <translation>ワイヤレスを切断</translation>
+    </message>
+    <message>
+      <source>Start Config</source>
+      <translation>構成を開始</translation>
+    </message>
+    <message>
+      <source>select path</source>
+      <translation>パスを選択</translation>
+    </message>
+    <message>
+      <source>record format：</source>
+      <translation>録画の形式:</translation>
+    </message>
+    <message>
+      <source>record screen</source>
+      <translation>画面を録画</translation>
+    </message>
+    <message>
+      <source>frameless</source>
+      <translation>フレームレス</translation>
+    </message>
+    <message>
+      <source>Use Simple Mode</source>
+      <translatorcomment>シンプルモードを使用する</translatorcomment>
+      <translation>シンプルモードを使用する</translation>
+    </message>
+    <message>
+      <source>Simple Mode</source>
+      <translatorcomment>シンプルモード</translatorcomment>
+      <translation>シンプルモード</translation>
+    </message>
+    <message>
+      <source>WIFI Connect</source>
+      <translatorcomment>Wi-Fi 接続</translatorcomment>
+      <translation>Wi-Fi 接続</translation>
+    </message>
+    <message>
+      <source>USB Connect</source>
+      <translatorcomment>USB 接続</translatorcomment>
+      <translation>USB 接続</translation>
+    </message>
+    <message>
+      <source>Double click to connect:</source>
+      <translation>ダブルクリックで接続:</translation>
+    </message>
+    <message>
+      <source>lock orientation:</source>
+      <translation>画面方向をロック:</translation>
+    </message>
+    <message>
+      <source>show fps</source>
+      <translation>FPS を表示</translation>
+    </message>
+    <message>
+      <source>stay awake</source>
+      <translation>画面を常時点灯</translation>
+    </message>
+    <message>
+      <source>device name:</source>
+      <translatorcomment>デバイス名:</translatorcomment>
+      <translation>デバイス名:</translation>
+    </message>
+    <message>
+      <source>update name</source>
+      <translatorcomment>更新名</translatorcomment>
+      <translation>更新名</translation>
+    </message>
+    <message>
+      <source>stop all server</source>
+      <translation>すべてのサーバーを停止</translation>
+    </message>
+    <message>
+      <source>adb command:</source>
+      <translation>adb コマンド:</translation>
+    </message>
+    <message>
+      <source>terminate</source>
+      <translation>停止</translation>
+    </message>
+    <message>
+      <source>execute</source>
+      <translation>実行</translation>
+    </message>
+    <message>
+      <source>clear</source>
+      <translation>消去</translation>
+    </message>
+    <message>
+      <source>reverse connection</source>
+      <translation>リバース接続</translation>
+    </message>
+    <message>
+      <source>background record</source>
+      <translation>バックグラウンド録画</translation>
+    </message>
+    <message>
+      <source>screen-off</source>
+      <translation>画面を OFF</translation>
+    </message>
+    <message>
+      <source>apply</source>
+      <translation>適用</translation>
+    </message>
+    <message>
+      <source>max size:</source>
+      <translation>最大サイズ:</translation>
+    </message>
+    <message>
+      <source>always on top</source>
+      <translation>常に手前に表示</translation>
+    </message>
+    <message>
+      <source>refresh script</source>
+      <translation>スクリプトを更新</translation>
+    </message>
+    <message>
+      <source>get device IP</source>
+      <translation>デバイス IP を取得</translation>
+    </message>
+    <message>
+      <source>USB line</source>
+      <translation>USB ライン</translation>
+    </message>
+    <message>
+      <source>stop server</source>
+      <translation>サーバーを停止</translation>
+    </message>
+    <message>
+      <source>start server</source>
+      <translation>サーバーを開始</translation>
+    </message>
+    <message>
+      <source>device serial:</source>
+      <translation>デバイスシリアル:</translation>
+    </message>
+    <message>
+      <source>bit rate:</source>
+      <translation>ビットレート:</translation>
+    </message>
+    <message>
+      <source>start adbd</source>
+      <translation>adbd を開始</translation>
+    </message>
+    <message>
+      <source>refresh devices</source>
+      <translation>デバイスを更新</translation>
+    </message>
+    <message>
+      <source>install sndcpy</source>
+      <translation>Sndcpy をインストール</translation>
+    </message>
+    <message>
+      <source>start audio</source>
+      <translation>オーディオを開始</translation>
+    </message>
+    <message>
+      <source>stop audio</source>
+      <translation>オーディオを停止</translation>
+    </message>
+    <message>
+      <source>auto update</source>
+      <translation>自動更新</translation>
+    </message>
+    <message>
+      <source>show toolbar</source>
+      <translation>ツールバーを表示</translation>
+    </message>
+    <message>
+      <source>record save path:</source>
+      <translation>録画の保存先:</translation>
+    </message>
+  </context>
+</TS>
