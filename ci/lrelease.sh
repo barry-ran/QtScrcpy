286268360ab403090c0c7b151fe973b311962edb
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2025-07-23 16:35:00 +0900

    Update lrelease.sh

--- ci/lrelease.sh
+++ ci/lrelease.sh
@@ -1,3 +1,3 @@
 # https://doc.qt.io/qt-5/linguist-manager.html#lrelease
 # lrelease -help
-lrelease ./QtScrcpy/res/i18n/en_US.ts ./QtScrcpy/res/i18n/zh_CN.ts
+lrelease ./QtScrcpy/res/i18n/en_US.ts ./QtScrcpy/res/i18n/zh_CN.ts ./QtScrcpy/res/i18n/ja_JP.ts
