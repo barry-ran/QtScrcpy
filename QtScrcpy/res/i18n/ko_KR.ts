e27748f00e6ac80c083460fa974475a8e501b9cd
VenusGirl❤ <venusgirl@outlook.com>
2025-11-06 23:44:45 +0900

    Add Korean
    Add Korean

---
+++ QtScrcpy/res/i18n/ko_KR.ts
@@ -0,0 +1,321 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>show</source>
+        <translation>표시</translation>
+    </message>
+    <message>
+        <source>quit</source>
+        <translation>종료</translation>
+    </message>
+    <message>
+        <source>original</source>
+        <translation>원본</translation>
+    </message>
+    <message>
+        <source>no lock</source>
+        <translation>잠금 없음</translation>
+    </message>
+    <message>
+        <source>Notice</source>
+        <translation>알림</translation>
+    </message>
+    <message>
+        <source>Hidden here!</source>
+        <translation>여기에 숨겨져 있어요!</translation>
+    </message>
+    <message>
+        <source>select path</source>
+        <translation>경로 선택</translation>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation>기록 지우기</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>This software is completely open source and free. Use it at your own risk. You can download it at the following address:</source>
+        <translation>이 소프트웨어는 완전히 오픈 소스이며 무료입니다. 본인의 위험을 감수하고 사용하세요. 다음 주소로 다운로드할 수 있습니다:</translation>
+    </message>
+    <message>
+        <source>QuickMirror</source>
+        <translation>빠른미러</translation>
+    </message>
+    <message>
+        <source>If you need more professional batch control mirror software, you can try the following software:</source>
+        <translation>더 전문적인 일괄 제어 미러 소프트웨어가 필요하다면 다음 소프트웨어를 사용해 볼 수 있습니다:</translation>
+    </message>
+    <message>
+        <source>If you need more professional game keymap mirror software, you can try the following software:</source>
+        <translation>더 전문적인 게임 키맵 미러 소프트웨어가 필요하다면 다음 소프트웨어를 사용해 보세요:</translation>
+    </message>
+    <message>
+        <source>QuickAssistant</source>
+        <translation>빠른 도우미</translation>
+    </message>
+    <message>
+        <source>You can contact me with telegram &lt;https://t.me/+Ylf_5V_rDCMyODQ1&gt;</source>
+        <translation>텔레그램으로 연락주세요 &lt;https://t.me/+Ylf_5V_rDCMyODQ1&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ToolForm</name>
+    <message>
+        <source>Tool</source>
+        <translation>도구</translation>
+    </message>
+    <message>
+        <source>full screen</source>
+        <translation>전체 화면</translation>
+    </message>
+    <message>
+        <source>expand notify</source>
+        <translation>확장 알림</translation>
+    </message>
+    <message>
+        <source>touch switch</source>
+        <translation>터치 스위치</translation>
+    </message>
+    <message>
+        <source>close screen</source>
+        <translation>화면 닫기</translation>
+    </message>
+    <message>
+        <source>power</source>
+        <translation>전ㅇ</translation>
+    </message>
+    <message>
+        <source>volume up</source>
+        <translation>볼륨 높이기</translation>
+    </message>
+    <message>
+        <source>volume down</source>
+        <translation>볼륨 낮추기</translation>
+    </message>
+    <message>
+        <source>app switch</source>
+        <translation>앱 스위치</translation>
+    </message>
+    <message>
+        <source>menu</source>
+        <translation>메뉴</translation>
+    </message>
+    <message>
+        <source>home</source>
+        <translation>home</translation>
+    </message>
+    <message>
+        <source>return</source>
+        <translation>돌ㅇ가기</translation>
+    </message>
+    <message>
+        <source>screen shot</source>
+        <translation>스크린샷</translation>
+    </message>
+    <message>
+        <source>open screen</source>
+        <translation>화면 열기</translation>
+    </message>
+    <message>
+        <source>group control</source>
+        <translation>그룹 제어</translation>
+    </message>
+</context>
+<context>
+    <name>VideoForm</name>
+    <message>
+        <source>file does not exist</source>
+        <translation>파일이 존재하지 않습니다</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <source>Wireless</source>
+        <translation>무선</translation>
+    </message>
+    <message>
+        <source>wireless connect</source>
+        <translation>무선 연결</translation>
+    </message>
+    <message>
+        <source>wireless disconnect</source>
+        <translation>무선 연결 끊기</translation>
+    </message>
+    <message>
+        <source>Start Config</source>
+        <translation>시작 구성</translation>
+    </message>
+    <message>
+        <source>select path</source>
+        <translation>경로 선택</translation>
+    </message>
+    <message>
+        <source>record format：</source>
+        <translation>기록 형식：</translation>
+    </message>
+    <message>
+        <source>record screen</source>
+        <translation>화면 녹화</translation>
+    </message>
+    <message>
+        <source>frameless</source>
+        <translation>프레임 없음</translation>
+    </message>
+    <message>
+        <source>Use Simple Mode</source>
+        <translatorcomment>Use Simple Mode</translatorcomment>
+        <translation>간단한 모드 사용</translation>
+    </message>
+    <message>
+        <source>Simple Mode</source>
+        <translatorcomment>Simple Mode</translatorcomment>
+        <translation>간단한 모드</translation>
+    </message>
+    <message>
+        <source>WIFI Connect</source>
+        <translatorcomment>WIFI Connect</translatorcomment>
+        <translation>WIFI 연결</translation>
+    </message>
+    <message>
+        <source>USB Connect</source>
+        <translatorcomment>USB Connect</translatorcomment>
+        <translation>USB 연결</translation>
+    </message>
+    <message>
+        <source>Double click to connect:</source>
+        <translation>더블 클릭하여 연결:</translation>
+    </message>
+    <message>
+        <source>lock orientation:</source>
+        <translation>잠금 방향:</translation>
+    </message>
+    <message>
+        <source>show fps</source>
+        <translation>fps 표시</translation>
+    </message>
+    <message>
+        <source>stay awake</source>
+        <translation>깨어 있기</translation>
+    </message>
+    <message>
+        <source>device name:</source>
+        <translatorcomment>device name:</translatorcomment>
+        <translation>장치 이름:</translation>
+    </message>
+    <message>
+        <source>update name</source>
+        <translatorcomment>update name</translatorcomment>
+        <translation>업데이트 이름</translation>
+    </message>
+    <message>
+        <source>stop all server</source>
+        <translation>모든 서버 중지</translation>
+    </message>
+    <message>
+        <source>adb command:</source>
+        <translation>adb 명령:</translation>
+    </message>
+    <message>
+        <source>terminate</source>
+        <translation>끝내기</translation>
+    </message>
+    <message>
+        <source>execute</source>
+        <translation>실행</translation>
+    </message>
+    <message>
+        <source>clear</source>
+        <translation>지우기</translation>
+    </message>
+    <message>
+        <source>reverse connection</source>
+        <translation>역연결</translation>
+    </message>
+    <message>
+        <source>background record</source>
+        <translation>배경 기록</translation>
+    </message>
+    <message>
+        <source>screen-off</source>
+        <translation>화면 끄기</translation>
+    </message>
+    <message>
+        <source>apply</source>
+        <translation>적용</translation>
+    </message>
+    <message>
+        <source>max size:</source>
+        <translation>최대 크기:</translation>
+    </message>
+    <message>
+        <source>always on top</source>
+        <translation>항상 위에</translation>
+    </message>
+    <message>
+        <source>refresh script</source>
+        <translation>스크립트 새로 고침</translation>
+    </message>
+    <message>
+        <source>get device IP</source>
+        <translation>장치 IP 가져오기</translation>
+    </message>
+    <message>
+        <source>USB line</source>
+        <translation>USB 선</translation>
+    </message>
+    <message>
+        <source>stop server</source>
+        <translation>서버 중지</translation>
+    </message>
+    <message>
+        <source>start server</source>
+        <translation>서버 시작</translation>
+    </message>
+    <message>
+        <source>device serial:</source>
+        <translation>장치 직렬:</translation>
+    </message>
+    <message>
+        <source>bit rate:</source>
+        <translation>비트 전송률:</translation>
+    </message>
+    <message>
+        <source>start adbd</source>
+        <translation>adbd 시작</translation>
+    </message>
+    <message>
+        <source>refresh devices</source>
+        <translation>장치 새로 고침</translation>
+    </message>
+    <message>
+        <source>install sndcpy</source>
+        <translation>sndcpy 설치</translation>
+    </message>
+    <message>
+        <source>start audio</source>
+        <translation>오디오 시작</translation>
+    </message>
+    <message>
+        <source>stop audio</source>
+        <translation>오디오 중지</translation>
+    </message>
+    <message>
+        <source>auto update</source>
+        <translation>자동 업데이트</translation>
+    </message>
+    <message>
+        <source>show toolbar</source>
+        <translation>도구 모음 표시</translation>
+    </message>
+    <message>
+        <source>record save path:</source>
+        <translation>기록 저장 경로:</translation>
+    </message>
+</context>
+</TS>
